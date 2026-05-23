376a556260dc4419b56abb09a8d94ac48aa37cf5
Alekhya138 <allukrishna2006@gmail.com>
2026-05-23 18:36:21 +0530

    Fix port binding for Remnder Deployment

--- app.py
+++ app.py
@@ -72,4 +72,7 @@ def contact():
 # RUN APP
 # -------------------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the port from Render's environment, or default to 5000 locally
+    port = int(os.environ.get("PORT", 5000))
+    # Listen on 0.0.0.0 so Render can access it
+    app.run(host="0.0.0.0", port=port, debug=True)
